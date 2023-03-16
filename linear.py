00c63aee9c9a99f57c4b3d3734413ad6350ad026
Adrian <adriankl39@googlemail.com>
2023-03-16 15:48:32 +0100

    update linear models

--- linear.py
+++ linear.py
@@ -1,21 +1,26 @@
-import path
 import pandas as pd
 import numpy as np
+
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import linear_model
 from scipy.stats import kendalltau
 from matplotlib import pyplot as plt
 from sklearn.model_selection import KFold
+from pathlib import Path
+
+PSUPERTIME_PATH = Path(__file__).parent
+
+FIGURE_PATH = PSUPERTIME_PATH / "figures"
 
 
 def lm(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
 
     models = {
-        # linear_model.LinearRegression: {},
+        linear_model.LinearRegression: {},
         # linear_model.LassoCV: {"tol": 0.01},
         # linear_model.Lasso: {"alpha": 0.0001, "tol": 0.01},
-        linear_model.RidgeCV: {},
+        # linear_model.RidgeCV: {},
     }
 
     for model, args in models.items():
@@ -28,33 +33,38 @@ def lm(X, y):
 
         ktau = kendalltau(y_predicted, y_test)
         print(ktau)
-        plt.plot(y_predicted, y_test, linestyle="", marker="s")
-        plt.xlabel("Predicted Age [Yr]")
-        plt.ylabel("Age [Yr]")
-        plt.show()
+        # plt.plot(y_predicted, y_test, linestyle="", marker="s")
+        # plt.xlabel("Predicted Age [Yr]")
+        # plt.ylabel("Age [Yr]")
+        # plt.show()
         # print(classifier.alphas_)
+    return ktau[0]
 
 
 def cross_validation(X, y):
     results = []
-    kf = KFold(n_splits=6, shuffle=True)
-    alphas = np.logspace(-11, -4, 8)
+    kf = KFold(n_splits=2, shuffle=True)
+    alphas = np.logspace(-7, -3, 3)
     for alpha in alphas:
         for i, (train_index, test_index) in enumerate(kf.split(X)):
             classifier = linear_model.Lasso(alpha=alpha)
             classifier.fit(X[train_index], y[train_index])
             y_predicted = classifier.predict(X[test_index])
-            f, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
-            f.suptitle(f"alpha: {alpha}")
+            # f, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
+            # f.suptitle(f"alpha: {alpha}")
 
-            ax.scatter(y_predicted, y[test_index])
-            ax.set_ylabel("Age", fontdict={"size": 18, "weight": "bold"})
-            ax.set_xlabel("Predicted Age", fontdict={"size": 18, "weight": "bold"})
+            # ax.scatter(y_predicted, y[test_index])
+            # ax.set_ylabel("Age", fontdict={"size": 18, "weight": "bold"})
+            # ax.set_xlabel("Predicted Age", fontdict={"size": 18, "weight": "bold"})
             ktau = kendalltau(y_predicted, y[test_index])
-            results.append({"Fold": i, "alpha": alpha, "KTau": ktau})
-            plt.show()
+            results.append({"Fold": i, "alpha": alpha, "KTau": ktau[0]})
+            # plt.show()
     df = pd.DataFrame(results, columns=["Fold", "alpha", "KTau"])
-    print(df.sort_values(by="alpha"))
+    by_alpha = df[["alpha", "KTau"]].groupby("alpha")
+    mean = by_alpha.mean().reset_index()
+    sd = by_alpha.std().reset_index()
+
+    return mean, sd
 
 
 def xy_fromdf(df):
@@ -66,8 +76,8 @@ def xy_fromdf(df):
 
 if __name__ == "__main__":
     # paths to data
-    genes_path = path.Path("variable_genes.csv")
-    ages_path = path.Path("Ages.csv")
+    genes_path = PSUPERTIME_PATH / "variable_genes.csv"
+    ages_path = PSUPERTIME_PATH / "Ages.csv"
 
     # import data
     genes_df = pd.read_csv(genes_path, index_col=False, sep=",")
@@ -87,34 +97,17 @@ if __name__ == "__main__":
     X, y = xy_fromdf(df=joined_df)
 
     # linear regression
-    # lm(X, y)
+    lin_ktau = lm(X, y)
 
-    cross_validation(X, y)
+    mean, sd = cross_validation(X, y)
 
-    # lasso = linear_model.Lasso(random_state=0, max_iter=10000)
-    # alphas = np.logspace(-4, -0.5, 30)
-    #
-    # tuned_parameters = [{"alpha": alphas}]
-    # n_folds = 5
-    #
-    # clf = GridSearchCV(lasso, tuned_parameters, cv=n_folds, refit=False)
-    # clf.fit(X, y)
-    # scores = clf.cv_results_["mean_test_score"]
-    # scores_std = clf.cv_results_["std_test_score"]
-    #
-    # plt.figure().set_size_inches(8, 6)
-    # plt.semilogx(alphas, scores)
-    #
-    # std_error = scores_std / np.sqrt(n_folds)
-    #
-    # plt.semilogx(alphas, scores + std_error, "b--")
-    # plt.semilogx(alphas, scores - std_error, "b--")
-    #
-    # # alpha=0.2 controls the translucency of the fill color
-    # plt.fill_between(alphas, scores + std_error, scores - std_error, alpha=0.2)
-    #
-    # plt.ylabel("CV score +/- std error")
-    # plt.xlabel("alpha")
-    # plt.axhline(np.max(scores), linestyle="--", color=".5")
-    # plt.xlim([alphas[0], alphas[-1]])
-    # plt.show()
+    f_alpha, ax_alpha = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
+    ax_alpha.errorbar(mean["alpha"], mean["KTau"], yerr=sd["KTau"], label="Lasso Regression $\pm$ SD")
+    ax_alpha.axhline(lin_ktau, linestyle="--", color="red", label="Linear Regression")
+
+    ax_alpha.set_xscale("log")
+    ax_alpha.set_ylim(0, 1)
+    ax_alpha.set_ylabel("Kendall's Tau [-]")
+    ax_alpha.set_xlabel("Alpha")
+    ax_alpha.legend()
+    plt.show()

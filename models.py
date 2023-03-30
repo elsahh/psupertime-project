60980a4ec9574f0fbd69f964c3d7126a81c1c8f4
Adrian <adriankl39@googlemail.com>
2023-03-30 15:09:06 +0200

    update figures

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ FIGURE_PATH = PSUPERTIME_PATH / 'figures'
 
 def data(genes_path):
     # path to ages
-    ages_path = PSUPERTIME_PATH / 'Ages.csv'
+    ages_path = PSUPERTIME_PATH / 'datasets' / 'Ages.csv'
 
     # import data
     genes_df = pd.read_csv(genes_path, index_col=False, sep=',')
@@ -44,32 +44,35 @@ def cross_validation_l1(X, y, olr_alphas, linear_alphas, n_folds):
     results = []
     for olr_alpha, linear_alpha in zip(olr_alphas, linear_alphas):
         # models
-        models = {
-            "olrl1": OrdinalClassifier(
-                clf=LogisticRegression(
-                    penalty='l1',
-                    solver='liblinear',
-                    max_iter=5000,
-                    C=1/olr_alpha
-                )
-            ),
-
-            "linearl1": Lasso(
-                tol=0.01,
-                alpha=linear_alpha
+        olrl1 = OrdinalClassifier(
+            clf=LogisticRegression(
+                penalty='l1',
+                solver='liblinear',
+                max_iter=5000,
+                C=1/olr_alpha
             )
-        }
+        )
 
-        for key, model in models.items():
-            for i, (train_index, test_index) in enumerate(kf.split(X)):
-                # fit model
-                model.fit(X[train_index], y[train_index])
-                # predicted test data
-                y_predicted = model.predict(X[test_index])
-                # Kendall's Tau between predicted age and measured age
-                ktau = kendalltau(y_predicted, y[test_index])
+        linearl1 = Lasso(
+            tol=0.01,
+            alpha=linear_alpha
+        )
 
-                results.append({'Model': key, 'Fold': i+1, 'Alpha': alpha, 'KTau': ktau[0]})
+        for i, (train_index, test_index) in enumerate(kf.split(X)):
+            # fit models
+            olrl1.fit(X[train_index], y[train_index])
+            linearl1.fit(X[train_index], y[train_index])
+
+            # predicted test data
+            y_predicted_olr = olrl1.predict(X[test_index])
+            y_predicted_linear = linearl1.predict(X[test_index])
+
+            # Kendall's Tau between predicted age and measured age
+            ktau_olr = kendalltau(y_predicted_olr, y[test_index])
+            ktau_linear = kendalltau(y_predicted_linear, y[test_index])
+
+            results.append({'Model': "olrl1", 'Fold': i+1, 'Alpha': olr_alpha, 'KTau': ktau_olr[0]})
+            results.append({'Model': "linearl1", 'Fold': i+1, 'Alpha': linear_alpha, 'KTau': ktau_linear[0]})
 
     results_df = pd.DataFrame(results, columns=['Model', 'Fold', 'Alpha', 'KTau'])
 
@@ -110,9 +113,7 @@ def crossvalidation_noreg(X, y, n_folds):
     return results_df
 
 
-def figure(title, results_noreg, results_l1):
-    print(results_noreg, "\n", results_l1)
-
+def figure(filename, results_noreg, results_l1):
     # mean, sd of noreg results
     noreg_by_model = results_noreg.groupby("Model")
     noreg_means = noreg_by_model.mean()
@@ -157,13 +158,11 @@ def figure(title, results_noreg, results_l1):
     )
 
     # plot alpha scan
-    print(results_l1)
     for model, ax in zip(["olrl1", "linearl1"], [ax_olr, ax_linear]):
         results_model = results_l1[results_l1.Model == model]
         by_alpha = results_model.groupby("Alpha")
         means = by_alpha.mean().reset_index()
         sds = by_alpha.std().reset_index()
-        print(means, sds)
         ax.errorbar(
             x=means['Alpha'],
             y=means['KTau'],
@@ -178,31 +177,33 @@ def figure(title, results_noreg, results_l1):
             color='black'
         )
 
-    # get x limits
-    min_alpha = results_l1["Alpha"].min()
-    max_alpha = results_l1["Alpha"].max()
+        # get x limits
+        min_alpha = results_model["Alpha"].min()
+        max_alpha = results_model["Alpha"].max()
 
-    x_min = min_alpha - min_alpha / 2
-    x_max = max_alpha + max_alpha / 2
+        x_min = min_alpha - min_alpha / 2
+        x_max = max_alpha + max_alpha / 2
+
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(0, 1)
+
+        ax.set_xscale('log')
 
     ax_linear.set_title("Linear Model")
     ax_olr.set_title("OLR Model")
 
     for ax in [ax_linear, ax_olr]:
-        ax.set_xscale('log')
-        ax.set_xlim(x_min, x_max)
-        ax.set_ylim(0, 1)
         ax.set_ylabel("Kendall's Tau [-]")
         ax.set_xlabel('alpha [-]')
         ax.legend()
-    plt.savefig(f"{title}.svg", dpi=300)
+    plt.savefig(PSUPERTIME_PATH / 'figures' / f"{filename}.svg", dpi=300)
     plt.show()
 
 
 if __name__ == '__main__':
     gene_paths = {
-        "500genes": PSUPERTIME_PATH / '500_variable_genes.csv',
-        "50genes": PSUPERTIME_PATH / '50_variable_genes.csv',
+        "500genes": PSUPERTIME_PATH / 'datasets' / '500_variable_genes.csv',
+        "50genes": PSUPERTIME_PATH / 'datasets' / '50_variable_genes.csv',
     }
 
     for dset_key, path in gene_paths.items():
@@ -210,24 +211,26 @@ if __name__ == '__main__':
         features, labels = data(genes_path=path)
 
         # params for cv
-        n_folds = 2
-        n_alphas = 3
+        n_folds = 5
+        n_alphas = 8
         alphass = {
-            "olr": {
-                "Fig1": (-7, -1),
-                "Fig2": (-5, -4),
+            "low_resolution": {
+                "olr": np.logspace(-7, -1, n_alphas),
+                "linear": np.logspace(-7, -1, n_alphas),
             },
-            "linear": {
-                "Fig1": (-7, -1),
-                "Fig2": (-5, -4),
+            "high_resolution": {
+                "olr": np.logspace(-5, -2.5, n_alphas),
+                "linear": np.logspace(-5.5, -4, n_alphas),
             },
         }
 
-        for title, alpha_limits in alphass.items():
+        for resolution, alphas in alphass.items():
             # cv of l1 regularized models
             results_l1 = cross_validation_l1(
                 X=features,
                 y=labels,
+                olr_alphas=alphas["olr"],
+                linear_alphas=alphas["linear"],
                 n_folds=n_folds,
             )
 
@@ -238,4 +241,8 @@ if __name__ == '__main__':
                 n_folds=n_folds,
             )
 
-            figure(title=title, results_noreg=results_noreg, results_l1=results_l1)
+            figure(
+                filename=f"Fig_{dset_key}_{resolution}",
+                results_noreg=results_noreg,
+                results_l1=results_l1
+            )
